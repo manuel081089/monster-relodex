c87f1c4304896d6dd184ad217b4a36218a9938df
Manuel Perez Torres <manuel081089@gmail.com>
2020-05-23 22:55:45 -0400

    Updates

---
+++ precache-manifest.9cd53a4528612fa4fab9f5db4587cb1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ff7ea9440305ee6ccc5014fe037726",
+    "url": "/monster-relodex/index.html"
+  },
+  {
+    "revision": "70531af7abcf7f71fd87",
+    "url": "/monster-relodex/static/css/main.1bd51ff8.chunk.css"
+  },
+  {
+    "revision": "449ebed2b5b4fb829c17",
+    "url": "/monster-relodex/static/js/2.46858d1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-relodex/static/js/2.46858d1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70531af7abcf7f71fd87",
+    "url": "/monster-relodex/static/js/main.11575590.chunk.js"
+  },
+  {
+    "revision": "6713cab3eb2da1b35875",
+    "url": "/monster-relodex/static/js/runtime-main.c1f6bd86.js"
+  }
+]);
